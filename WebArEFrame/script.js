2d9cceeaeb403977496ad9acac0bc0a0b723f4b1
jasonesquivel <95312146+jasonesquivel@users.noreply.github.com>
2023-05-02 04:05:10 -0600

    mensajes personalizados por imagen

--- WebArEFrame/script.js
+++ WebArEFrame/script.js
@@ -1,10 +1,10 @@
-const imagen = document.querySelector('#imagen1');
-imagen.addEventListener('click', () => {
-  // Código para mostrar el mensaje flotante
-});
+const imagen1 = document.querySelector('#imagen1');
 const mensaje = document.querySelector('#mensaje');
 
-imagen.addEventListener('click', () => {
+imagen1.addEventListener('click', () => {
+  const messageText = imagen1.getAttribute('data-message');
+  const messageHtml = `<h2>Información sobre la imagen</h2><p>${messageText}</p>`;
+  mensaje.innerHTML = messageHtml;
   mensaje.classList.remove('oculto');
 });
 
@@ -12,5 +12,30 @@ mensaje.addEventListener('click', () => {
   mensaje.classList.add('oculto');
 });
 
+const imagen2 = document.querySelector('#imagen2');
+const mensaje2 = document.querySelector('#mensaje');
 
+imagen2.addEventListener('click', () => {
+  const messageText = imagen2.getAttribute('data-message');
+  const messageHtml = `<h2>Información sobre la imagen</h2><p>${messageText}</p>`;
+  mensaje.innerHTML = messageHtml;
+  mensaje.classList.remove('oculto');
+});
 
+mensaje.addEventListener('click', () => {
+  mensaje.classList.add('oculto');
+});
+
+const imagen3 = document.querySelector('#imagen3');
+const mensaje3 = document.querySelector('#mensaje');
+
+imagen3.addEventListener('click', () => {
+  const messageText = imagen3.getAttribute('data-message');
+  const messageHtml = `<h2>Información sobre la imagen</h2><p>${messageText}</p>`;
+  mensaje.innerHTML = messageHtml;
+  mensaje.classList.remove('oculto');
+});
+
+mensaje.addEventListener('click', () => {
+  mensaje.classList.add('oculto');
+});
